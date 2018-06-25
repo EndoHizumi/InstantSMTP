57ca624e50d94f3a3dd2e9b4e8058f3bbb2d7639
EndoHizumi <winvissoftware@gmail.com>
2018-06-25 22:42:12 +0900

    add:EHLO.Tests.ps1

---
+++ functions/EHLO.Tests.ps1
@@ -0,0 +1,16 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+$clientDomain=""
+Describe "EHLO" {
+    $returnMsg=EHLO "abc.com:114514"
+    It "Return 250 OK" {
+        $returnMsg | Should -Be "250 OK"
+    }
+    It "`$clientDomain is abc.com:114514" {
+        $clientDomain | Should -Be "abc.com:114514"
+    }
+    It "EHLO Args is Null" {
+        EHLO| Should -Be "250 OK"
+    }
+}
